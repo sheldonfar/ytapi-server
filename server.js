05dc130994cab8135a62ef6837937eb6c6b283a2
Eugene <sheldonfar@gmail.com>
2015-11-28 21:09:14 +0200

    fixed port troubles

--- server.js
+++ server.js
@@ -159,11 +159,11 @@ app.get('/api/customquery', function (req, res) {
     });
 });
 
-var servPort = process.env.PORT || 8888;
-var server = app.listen(servPort, function () {
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8888;
+var server = app.listen(port, ipaddress, function () {
 
     var host = server.address().address;
-    var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port)
 
